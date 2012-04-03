d7d719cd4b379574598c7460c475ff8e76481f97
Dmitry Gruzd <donotsendhere@gmail.com>
2012-04-03 13:50:50 +0400

    some assets stuff

--- lib/capistrano/ext/capobvious.rb
+++ lib/capistrano/ext/capobvious.rb
@@ -50,7 +50,7 @@ Capistrano::Configuration.instance.load do
   before "deploy:restart", "auto:run"
   #after "deploy:setup", "db:create", "nginx:conf", "install:p7zip"
 
-
+#load 'deploy/assets'
   namespace :auto do
     task :run do
       if exists?(:assets) && fetch(:assets) == true
@@ -215,12 +215,16 @@ Capistrano::Configuration.instance.load do
   end
 
   namespace :assets do
-    desc "Assets precompile"
-    task :precompile do
+    desc "Local Assets precompile"
+    task :local_precompile do
       system("bundle exec rake assets:precompile && cd public && tar czf assets.tar.gz assets/")
       upload("public/assets.tar.gz","#{current_path}/public/assets.tar.gz")
       system("rm public/assets.tar.gz && rm -rf tmp/assets && mv public/assets tmp/assets")
       run("cd #{current_path}/public && rm -rf assets/ && tar xzf assets.tar.gz && rm assets.tar.gz")
+    end
+    desc "Assets precompile"
+    task :server_precompile, :roles => :web, :except => { :no_release => true } do
+      run("cd #{current_path} && bundle exec rake RAILS_ENV=#{env} assets:precompile")
     end
   end
 
