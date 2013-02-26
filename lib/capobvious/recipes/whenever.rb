4b58e1e972d01672a8524a2f001a3dbe3a7db25c
Dmitry Gruzd <donotsendhere@gmail.com>
2013-02-26 11:57:16 +0400

    new version

--- lib/capobvious/recipes/whenever.rb
+++ lib/capobvious/recipes/whenever.rb
@@ -1,6 +1,8 @@
 Capistrano::Configuration.instance(:must_exist).load do
   if exists?(:whenever) && fetch(:whenever) == true
     set :whenever_command, "bundle exec whenever"
+    set :whenever_environment, defer { stage }
+    set :whenever_identifier, defer { "#{application}_#{stage}" }
     require "whenever/capistrano/recipes"
     after "bundle:install", "whenever:update_crontab"
     after "deploy:rollback", "whenever:update_crontab"
--- lib/capobvious/version.rb
+++ lib/capobvious/version.rb
@@ -1,3 +1,3 @@
 module Capobvious
-  VERSION = "0.3.pre4"
+  VERSION = "0.3.pre5"
 end
